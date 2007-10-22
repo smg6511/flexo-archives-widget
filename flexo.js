fe6bf18c0f6df11ac3694fff04e7900c21241e82
heathharrelson <heathharrelson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-10-22 04:41:30 +0000

    Changes to have only one list open at a time
    git-svn-id: https://plugins.svn.wordpress.org/flexo-archives-widget/trunk@22789 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flexo.js
+++ flexo.js
@@ -2,7 +2,7 @@
 /*
  * flexo.js by Heath Harrelson, Copyright (C) 2007
  *
- * Version: 1.1.2
+ * Version: 1.1.3
  * 
  * Expands and collapses menus.  Used by the Flexo Archives WordPress widget
  * (http://www.pointedstick.net/heath/flexo-archives-widget).
@@ -29,10 +29,14 @@
 
 var flexoToggle = {
 	widgetRoot : null,   // The widget's root element
+	linksArray : null,   // The flexo-links
+	listsArray : null,   // The flexo-lists
 
 	init : function () {
 		var yearLinks;   // List of all the expandable links
+		var yearLists;   // List of month lists
 		var numLinks;    // Number of elements in yearLinks
+		var numLists;    // Number of elements in yearLists
 		var widget;      // Outermost container of the widget
 
 		// Dom support or bust!
@@ -41,7 +45,7 @@ var flexoToggle = {
 
 		// If the widget's root can't be found, we can't
 		// do anything.
-		this.findWidgetRoot();
+		this.findWidgetElements();
 		if (!this.widgetRoot) {
 			return;
 		} else {
@@ -49,12 +53,16 @@ var flexoToggle = {
 		}
 
 		// Get a list of all the expandable links
-		yearLinks = this.getElementByClassName(widget, 'a', 'flexo-link');
+		yearLinks = this.linksArray;
 		numLinks = yearLinks.length;
 
+		// Get the list of all month lists
+		yearLists = this.listsArray;
+		numLists = yearLists.length;
+
 		// Hide each list of months
-		for (var i = 0; i < numLinks; i++) {
-			this.setStateForListWithLink(yearLinks[i]);
+		for (var i = 0; i < numLists; i++) {
+			this.setDisplay(yearLists[i]);
 		}
 
 		// Add a hint to each expandable link
@@ -99,10 +107,13 @@ var flexoToggle = {
 		return true;
 	},
 
-	// Show or hide the list associated with element aLink.
+	// Show or hide the list associated with element aLink,
+	// hiding all other lists.
 	setStateForListWithLink : function (aLink) {
 		var startOfMenu;    // Element where archive list starts
-		var flexLists;      // Array of month lists
+		var thisList;       // Month list for this link
+		var allLists;       // Array of month lists
+		var expanded;       // Is the list for this month expanded
 		var len;            // The number of flexLists
 
 		// Grandparent should be start of archive list
@@ -114,39 +125,62 @@ var flexoToggle = {
 		    !flexoToggle.isListElement(startOfMenu))
 			return false;
 
-		// Get list(s) of months
-		flexLists = flexoToggle.getElementByClassName(startOfMenu,
-				'ul', 'flexo-list');
+		// Get month list associated with link clicked
+		thisList = flexoToggle.getElementByClassName(startOfMenu,
+								'ul', 'flexo-list');
 
-		if (flexLists.length > 0) {
-			len = flexLists.length;
+		// See whether we're contracting an open list
+		if (thisList[0] && thisList[0].style.display == 'block')
+			expanded = true;
+
+		// Hide any open lists, so only one is open after return
+		allLists = flexoToggle.listsArray;
+        len = allLists.length;
+		for (var i = 0; i < len; i++) {
+			if (allLists[i].style.display == 'block' ||
+			    allLists[i].style.display == '')
+				flexoToggle.setDisplay(allLists[i]);
+		}
+
+		// Show the month list for link clicked if it was collapsed
+		if (!expanded) {
+			len = thisList.length;
 			for (var i = 0; i < len; i++)
-				flexoToggle.setDisplay(flexLists[i]);
+				flexoToggle.setDisplay(thisList[i]);
 		}
 
 		return true;
 	},
 
 	// Find the root element of the widget.
-	findWidgetRoot : function () {
-		// Nice themes will supply the ID of the widget.
-		var el = document.getElementById('flexo-archives');
-		var flexLinks;
+	findWidgetElements : function () {
+		// Nice themes will supply the widget with an ID.
+		var rootElement = document.getElementById('flexo-archives');
 
-		// This theme is not so nice. :(
-		if (!el) {
-			// Get the list of flexo-links
-			flexLinks = flexoToggle.getElementByClassName(document, 'a',
+		// Find all the flexo-links
+		var flexLinks = flexoToggle.getElementByClassName(document, 'a',
 									'flexo-link');
 
-			// The widget's root is the great-grandparent of the
-			// first flexo-link
-			el = flexLinks[0].parentNode.parentNode.parentNode;
+		// Find all the flexo-lists
+		var flexLists = flexoToggle.getElementByClassName(document, 'ul',
+									'flexo-list');
+
+		// This theme is not so nice. :(
+		if (!rootElement) {
+			// The widget's root should be the great-grandparent
+			// of the first flexo-link
+			rootElement = flexLinks[0].parentNode.parentNode.parentNode;
 		}
 
-		// Set class variable
-		if (el)
-			flexoToggle.widgetRoot = el;
+		// Set class variables
+		if (rootElement)
+			flexoToggle.widgetRoot = rootElement;
+
+		if (flexLinks)
+			flexoToggle.linksArray = flexLinks;
+
+		if (flexLists)
+			flexoToggle.listsArray = flexLists;
 	},
 
 	// Determine if DOM element el is a list (ul, ol)
